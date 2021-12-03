29ac9e65a972e38f9d52fe579345dc29c0e39f36
TonyTsai <tony427@gmail.com>
2021-12-03 16:55:22 +0800

    format log output as json.

--- log4netDemo/Controllers/HomeController.cs
+++ log4netDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using log4net;
+using System;
 using System.Web.Mvc;
 
 namespace log4netDemo.Controllers
@@ -9,10 +10,23 @@ namespace log4netDemo.Controllers
 
         public ActionResult Index()
         {
-            log.Debug("Debug");
-            log.Info("Info");
-            log.Warn("Warn");
-            log.Error("Error");
+            dynamic debug = new { id = 1, MEssage = "Debug" };
+            log.Debug(debug);
+
+            dynamic info = new { id = 2, MEssage = "Info" };
+            log.Info(info);
+
+            dynamic warn = new { id = 3, MEssage = "Warn" };
+            log.Warn(warn);
+
+            try
+            {
+                throw new Exception("throw new exception");
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+            }
 
             return View();
         }
